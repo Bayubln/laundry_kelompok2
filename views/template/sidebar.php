0005d00e4c3db48f3fe565fdf3a640d10fa772f9
Bayubln <124258967+Bayubln@users.noreply.github.com>
2023-02-16 15:19:51 +0700

    inisialisasi project

---
+++ views/template/sidebar.php
@@ -0,0 +1,74 @@
+<body class="nav-md">
+    <div class="container body">
+      <div class="main_container">
+        <div class="col-md-3 left_col">
+          <div class="left_col scroll-view">
+            <div class="navbar nav_title" style="border: 0;">
+              <a href="index.html" class="site_title"><i class="fa fa-paw"></i> <span>Aplikasi Laundry</span></a>
+            </div>
+
+            <div class="clearfix"></div>
+
+            <!-- menu profile quick info -->
+            <div class="profile clearfix">
+              <div class="profile_pic">
+                <img src="../../assets/images/jjj.jpg.jpg" alt="..." class="img-circle profile_img">
+              </div>
+              <div class="profile_info">
+                <span>Welcome,</span>
+                <h2>Ajul Laundry</h2>
+              </div>
+            </div>
+            <!-- /menu profile quick info -->
+
+            <br />
+
+            <!-- sidebar menu -->
+            <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+              <div class="menu_section">
+                <h3>General</h3>
+                <ul class="nav side-menu">                  
+                  <li><a href="#"><i class="fa fa-home"></i> Home <span class="label label-success pull-right"></span></a></li>
+                
+                  <li><a href="#"><i class="fa fa-user"></i> Pelanggan  <span class="label label-success pull-right"></span></a></li>
+
+                  <li><a><i class="fa fa-group"></i> Pengguna <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="../pengguna/v_list_pengguna.php">Daftar Pengguna</a></li>
+                      <li><a href="../pengguna/v_tambah_pengguna.php">Tambah Pengguna</a></li>
+                    </ul>
+                  </li>
+
+                  <li><a><i class="fa fa-home"></i> Outlet <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="../outlet/v_list_outlet.php">Daftar Outlet</a></li>
+                      <li><a href="../outlet/v_tambah_outlet.php">Tambah Oulet</a></li>
+                    </ul>
+                  </li>
+
+                  <li><a href="#"><i class="fa fa-money"></i> Transaksi <span class="label label-success pull-right"></span></a></li>
+
+                  <li><a href="#"><i class="fa fa-group"></i> Registrasi Pelanggan <span class="label label-success pull-right"></span></a></li>
+
+                  <li><a><i class="fa fa-edit"></i> Produk <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="../produk/v_list_produk.php">Daftar Produk</a></li>
+                      <li><a href="../produk/v_tambah_produk.php">Tambah Produk</a></li>
+                    </ul>
+                  </li>
+
+                  <!-- <li><a><i class="fa fa-clone"></i>Layouts <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="fixed_sidebar.html">Fixed Sidebar</a></li>
+                      <li><a href="fixed_footer.html">Fixed Footer</a></li>
+                    </ul>
+                  </li> -->
+
+                </ul>
+
+              </div>
+
+            </div>
+            <!-- /sidebar menu -->
+          </div>
+        </div>
